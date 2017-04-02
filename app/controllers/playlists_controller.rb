9d5527cec39c2061862a0fe8108ef50b672b72ce
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-04-01 18:54:11 -0600

    refactored with before action and error partial

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,11 +1,13 @@
 class PlaylistsController < ApplicationController
+  before_action :set_playlist, only: [:show, :edit]
+  before_action :set_songs, only: [:new, :edit]
+
   def index
     @playlists  = Playlist.all
   end
 
   def new
     @playlist = Playlist.new
-    @songs = Song.all
   end
 
   def create
@@ -14,20 +16,28 @@ class PlaylistsController < ApplicationController
   end
 
   def show
-    @playlist = Playlist.find(params[:id])
   end
 
   def edit
-    @playlist = Playlist.find(params[:id])
-    @songs = Song.all
   end
 
   def update
-    @playlist = Playlist.update(playlist_params)
-    redirect_to playlist_path
+    if @playlist = Playlist.update(playlist_params)
+      redirect_to playlist_path
+    else
+      render :edit
+    end
   end
 
   private
+
+  def set_songs
+    @songs = Song.all
+  end
+
+  def set_playlist
+    @playlist = Playlist.find(params[:id])
+  end
 
   def playlist_params
     params.require(:playlist).permit(:name, song_ids: [])
