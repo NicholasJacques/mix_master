b50ca3fd3a0ee02cef464891e6cd5181e0319325
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-04-02 16:34:20 -0600

    completed iteration 7"

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,8 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+OmniAuth.config.test_mode = true
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
